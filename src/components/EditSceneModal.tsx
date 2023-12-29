5addfc6f6e4ec4f2d7d94229044513f7c2c211fc
averman <averman@gmail.com>
2023-12-29 18:54:43 +0700

    Character Perception of Other

--- src/components/EditSceneModal.tsx
+++ src/components/EditSceneModal.tsx
@@ -4,6 +4,11 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/esm/Card';
 import Form from 'react-bootstrap/Form';
 import { SceneSummary } from '../models/SceneSummary';
+import db from '../utils/Db';
+import { characterPerceptionAgent } from '../agents/CharacterPerceptionAgent';
+import { Chub } from '../accessor/Chub';
+import { getAiCompletions } from '../PropsTransformer/AiAgentsTransformer';
+import { AiCompletion } from '../accessor/AiCompletion';
 
 interface EditSceneModalProps {
     show: string | null;
@@ -12,6 +17,18 @@ interface EditSceneModalProps {
     onSave: (newSummary: SceneSummary, key: string) => void;
     summarizeScene: (key: string) => void;
 }
+
+let ais: {[key: string]: AiCompletion} = {};
+
+let rawSetting = localStorage.getItem('settings');
+if(rawSetting) {
+    let settings = JSON.parse(rawSetting).settings;
+    if(settings.apis) {
+        ais = getAiCompletions(settings);
+    }
+}
+
+let aiAgent = new characterPerceptionAgent(ais.mars);
 
 const EditSceneModal: React.FC<EditSceneModalProps> = ({ show, onHide, sceneSummary, onSave, summarizeScene }) => {
     const [newSceneSummary, setNewSceneSummary] = useState<SceneSummary>(Object.assign({}, sceneSummary));
@@ -39,6 +56,48 @@ const EditSceneModal: React.FC<EditSceneModalProps> = ({ show, onHide, sceneSumm
         } catch (err) {
             console.log(err);
         }
+    }
+
+    async function updateCharacterPerception(scenePath: string): Promise<void> {
+        let [messages, scenes] = await Promise.all([db.messages.toArray(), db.scenes.toArray()]);
+        let scenesList = messages.map(m => m.scenes.join(','))
+            .reduce((acc, val) => acc.includes(val) ? acc : acc.concat(val), [] as string[]);
+        scenesList = scenesList.slice(0, scenesList.indexOf(scenePath));
+        let characterPerceptionBefore: {[key: string]: {[key2: string]: string}} = {};
+        for(let s of scenesList) {
+            let scene = scenes.find(sc => sc.scenePath === s);
+            if(scene && scene.characterPerception) {
+                for(let c in scene.characterPerception) {
+                    if(!characterPerceptionBefore[c]) characterPerceptionBefore[c] = {};
+                    for(let c2 in scene.characterPerception[c]) {
+                        characterPerceptionBefore[c][c2] = scene.characterPerception[c][c2];
+                    }
+                }
+            }
+        }
+        
+        let messagesInScene = messages.filter(m => m.scenes.join(',') === scenePath);
+        let currentScene = scenes.find(s => s.scenePath === scenePath);
+        let charactersInScene = messagesInScene.map(m => m.sender)
+                                    .reduce((acc, val) => acc.includes(val) ? acc : acc.concat(val), [] as string[])
+                                    .map(c=> c == 'user' ? c : c.split(':')[1]);
+        let messagesInAllScene = messages.filter(m => (m.scenes.join(',') === scenePath) || (m.scenes.join(',').startsWith(scenePath+",")));
+        let charactersInAllScene = messagesInAllScene.map(m => m.sender)
+                                    .reduce((acc, val) => acc.includes(val) ? acc : acc.concat(val), [] as string[])
+                                    .map(c=> c == 'user' ? c : c.split(':')[1]);
+
+        let newCharacterPerception: {[key: string]: {[key2: string]: string}} = {};
+        for(let c of charactersInScene) {
+            if(c.trim() == 'user') continue;
+            for(let c2 of charactersInAllScene) {
+                if(c2.trim() == c.trim()) continue;
+                let response = await aiAgent.getPerception(c, c2, characterPerceptionBefore, messagesInScene);
+                if(!newCharacterPerception[c]) newCharacterPerception[c] = {};
+                newCharacterPerception[c][c2] = response;
+            }
+        }
+
+        setNewSceneSummary({ ...newSceneSummary, 'characterPerception': newCharacterPerception });
     }
 
     return (
@@ -116,6 +175,18 @@ const EditSceneModal: React.FC<EditSceneModalProps> = ({ show, onHide, sceneSumm
                                     value={JSON.stringify(newSceneSummary.sceneScenarioChanges) || ''}
                                     onChange={(e) => handleInputChange(e, 'sceneScenarioChanges')}
                                 />
+                            </Form.Group>
+
+                            {/* characterPerception Changes */}
+                            <Form.Group>
+                                <Form.Label>Character Perception Changes</Form.Label>
+                                <Form.Control
+                                    as="textarea"
+                                    rows={3}
+                                    value={JSON.stringify(newSceneSummary.characterPerception, null, 2) || ''}
+                                    onChange={(e) => handleJSONChange(e, 'characterPerception')}
+                                />
+                                <Button variant="primary" onClick={() => updateCharacterPerception(show!)}>Update Character Perception</Button>
                             </Form.Group>
 
                             {/* Scene Contexts */}
